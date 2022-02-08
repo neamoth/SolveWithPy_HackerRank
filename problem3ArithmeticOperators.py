1eb1d9fb8bfc7b715c7430e73b8f9dc90ed30db7
Neamoth <Neamothcse@gmail.com>
2022-02-08 13:01:39 +0600

    Basic: Arithmatic Operators Problem solved

---
+++ problem3ArithmeticOperators.py
@@ -0,0 +1,27 @@
+'''
+Source: url(https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true)
+SourceName: HackersRank
+
+Qustion of solving Arithmetic Operators
+
+'''
+
+# Solution:
+
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+
+# task1: Sum
+
+sum = a+b
+print(f"The sum of {a} and {b} is, {sum}")
+
+# task2: Difference
+
+min = a-b
+print(f"The difference between {a} and {b} is, {min} ")
+
+# task3: multification
+
+mult = a*b
+print(f"The multification of {a} and {b} is, {mult} ")
